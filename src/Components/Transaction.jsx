88bb48db1ed39bf3bdf53353acf8212c513f01b4
sa7vic <sourav86_soe@jnu.ac.in>
2025-06-23 10:20:20 +0530

    Update Transaction.jsx

--- src/Components/Transaction.jsx
+++ src/Components/Transaction.jsx
@@ -4,12 +4,23 @@ import { GlobalContext } from '../Context/GlobalState';
 export const Transaction = ({ transaction }) => {
     const { deleteTransaction } = useContext(GlobalContext);
 
+    const emojiMap = {
+        Groceries: "🛒",
+        Coffee: "☕",
+        Rent: "🏠",
+        Bills: "🧾",
+        Salary: "💼"
+    };
+
+    const emoji = emojiMap[transaction.text] || "";
+
+
     const sign = transaction.amount < 0 ? '-' : '+';
     return (
-        <li className={transaction.amount < 0 ? "minus" : "plus"}>
-            {transaction.text} 
-            <span>{sign}${Math.abs(transaction.amount)}</span>
-            <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
-        </li>
-    );
+  <li className={transaction.amount < 0 ? "minus" : "plus"}>
+    {emoji} {transaction.text}
+    <span>{sign}${Math.abs(transaction.amount)}</span>
+    <button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+  </li>
+);
 }
